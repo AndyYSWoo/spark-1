77459677ae2f171f7c8591079f2c6136747dd3d9
Andy <andyyswoo@gmail.com>
2016-10-28 11:41:00 +0800

    add script to automate build and submit jobs

---
+++ bin/run-proj.sh
@@ -0,0 +1,6 @@
+# build project
+cd /Users/yongshangwu/work/TestOnSpark
+mvn package
+# submit to spark
+cd /Users/yongshangwu/work/spark-latest/dist
+./bin/spark-submit --class me.yongshang.cbfm.sparktest.SparkTest --master local /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar

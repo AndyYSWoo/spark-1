68071178fb4822175180a2eec705c177e3f445d2
Andy <andyyswoo@gmail.com>
2016-11-01 20:43:41 +0800

    delete all *.parquet files on starting a new job

--- bin/run-proj.sh
+++ bin/run-proj.sh
@@ -3,6 +3,7 @@ cd /Users/yongshangwu/work/TestOnSpark
 mvn package
 # submit to spark
 cd /Users/yongshangwu/work/spark-latest/dist
-rm -rf persons.parquet
+rm -rf *.parquet
 export SPARK_HOME = "/Users/yongshangwu/work/spark-latest/dist"
 ./bin/spark-submit --class me.yongshang.cbfm.sparktest.SparkTest --master local /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar
+# ./bin/spark-submit --class me.yongshang.cbfm.sparktest.DataGenerator --master local /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar

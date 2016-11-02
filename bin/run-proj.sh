7a082f3e58766f68136afe8b94fe3412537ca0ca
Andy <andyyswoo@gmail.com>
2016-11-02 12:16:02 +0800

    submit to local[*]
    i.e. add executors to solve mem problem caused by snappy compressor

--- bin/run-proj.sh
+++ bin/run-proj.sh
@@ -5,5 +5,5 @@ mvn package
 cd /Users/yongshangwu/work/spark-latest/dist
 rm -rf *.parquet
 export SPARK_HOME = "/Users/yongshangwu/work/spark-latest/dist"
-./bin/spark-submit --class me.yongshang.cbfm.sparktest.SparkTest --master local /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar
-# ./bin/spark-submit --class me.yongshang.cbfm.sparktest.DataGenerator --master local /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar
+# ./bin/spark-submit --class me.yongshang.cbfm.sparktest.SparkTest --master local[*] /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar
+./bin/spark-submit --class me.yongshang.cbfm.sparktest.DataGenerator --master local[*] /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar

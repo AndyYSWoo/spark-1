3b2ace602373be83c3c1fcf79102b1f5d2c2b473
Andy <andyyswoo@gmail.com>
2016-11-04 14:20:40 +0800

    remove [*] in submit com

--- bin/run-proj.sh
+++ bin/run-proj.sh
@@ -3,7 +3,7 @@ cd /Users/yongshangwu/work/TestOnSpark
 mvn package
 # submit to spark
 cd /Users/yongshangwu/work/spark-latest/dist
-rm -rf *.parquet
 export SPARK_HOME = "/Users/yongshangwu/work/spark-latest/dist"
-# ./bin/spark-submit --class me.yongshang.cbfm.sparktest.SparkTest --master local[*] /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar
-./bin/spark-submit --class me.yongshang.cbfm.sparktest.DataGenerator --master local[*] /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar
+# rm -rf *.parquet && ./bin/spark-submit --class me.yongshang.cbfm.sparktest.SparkTest --master local[*] /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar
+# rm -rf *.parquet && ./bin/spark-submit --class me.yongshang.cbfm.sparktest.DataGenerator --master local /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar
+rm -rf *.parquet && ./bin/spark-submit --class me.yongshang.cbfm.sparktest.QueryTest --master local /Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar

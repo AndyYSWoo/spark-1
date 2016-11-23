e8e4dd1a4bad6f7f12ce353b8c51e427c37e7702
Andy <andyyswoo@gmail.com>
2016-11-23 17:55:26 +0800

    change record file structure

--- bin/run-proj.sh
+++ bin/run-proj.sh
@@ -1,10 +1,10 @@
-# use index: cbfm, mdbf
+# use index: cbfm, mdbf, cmdbf
 index="cbfm"
 # path def
 app_jar_file="/Users/yongshangwu/work/TestOnSpark/target/sparktest-1.0-SNAPSHOT.jar"
 data_source_folder="/Users/yongshangwu/Downloads/tpch_2_17_0/dbgen/"
-record_dir="/Users/yongshangwu/work/result/"$index"/"
-# recreate result record dir
+record_dir="/Users/yongshangwu/work/result/$index/$(date +%Y%m%d-%H%M%S)/"
+# create result record dir, delete first in case
 rm -rf $record_dir
 mkdir -p $record_dir
 # build project
@@ -17,4 +17,5 @@ export SPARK_HOME = "/Users/yongshangwu/work/spark-latest/dist"
 rm -rf *.parquet
 # ./bin/spark-submit --class me.yongshang.cbfm.sparktest.SparkTest --master local $app_jar_file
 #./bin/spark-submit --class me.yongshang.cbfm.sparktest.DataGenerator --master local $app_jar_file
-./bin/spark-submit --class me.yongshang.cbfm.sparktest.QueryTest --master local[*] --files ./conf/spark-defaults.conf $app_jar_file $data_source_folder $record_dir $index
+# ./bin/spark-submit --class me.yongshang.cbfm.sparktest.QueryTest --master local[*] --files ./conf/spark-defaults.conf $app_jar_file $data_source_folder $record_dir $index
+./bin/spark-submit --class me.yongshang.cbfm.sparktest.TpchQuery --master local --files ./conf/spark-defaults.conf $app_jar_file $data_source_folder $record_dir $index
